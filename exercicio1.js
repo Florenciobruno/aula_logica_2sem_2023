aeffbfd228a9d20ec5c3abbe59e15d0a4eedb75a
DevFlorencio <116380905+Florenciobruno@users.noreply.github.com>
2023-08-17 22:17:32 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,7 @@
+let nome_completo = "bruno da silva florencio";
+console.log("Olá, " + nome_completo + "! Bem vindo ao javascript.");
+
+let registro_de_aluno = ("04386711");
+console.log("seu registro de aluno é o numero: " + registro_de_aluno);
+let curso = ("Tecnologia em Sistemas para Internet");
+console.log("Você esta inscrito no curso de " + curso);
---
+++ exercicio2.js
@@ -0,0 +1,8 @@
+let idade = 29;
+console.log = ("Sua idade é de: " + idade);
+let peso = 78;
+console.log = ("Seu peso é de: " + peso);
+let sexo_masculino = m;
+let sexo_feminino = f;
+prompt("digite seu sexo")
+console.log = ("Você é do sexo: " +);
