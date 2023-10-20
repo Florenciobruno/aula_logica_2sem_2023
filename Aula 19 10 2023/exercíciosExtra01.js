b0935bdc0923bc5096a796460ea2f5ee62eeac2d
DevFlorencio <116380905+Florenciobruno@users.noreply.github.com>
2023-10-19 22:28:44 -0300

    Aula do dia 19 10 2023 de JavaScript

---
+++ Aula 19 10 2023/exercíciosExtra01.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<meta charset="UTF-8">
+			<title>Exercício 01</title>
+		</head>
+		
+		<body>
+
+			<script>
+				var entrada = parseInt(prompt("Digite um número para ser avaliado!"))
+
+				if(entrada >= 0)
+					document.write("Este é um número Positivo")
+				else{
+					document.write("Este é um número negativo")
+				}
+			</script>
+		</body>
+	</html>
